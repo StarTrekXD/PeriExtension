6a441d7d835c0a36a19bb663e771c7351210c6ea
PeriLake <69841536+PeriLake@users.noreply.github.com>
2021-03-20 19:46:32 +0300

    Update tempdb.js

--- src/tempdb.js
+++ src/tempdb.js
@@ -6,9 +6,11 @@ class TempDB {
         this.events = {};
         this.json = new JsonSerializer()
         setInterval(() => {
-            this.events.reset.forEach(a => {
-                a(this.json.json);
-            });
+            if (this.events["reset"]) {
+                this.events["reset"].forEach(a => {
+                    a(this.json.json);
+                });
+            }
             this.json = new JsonSerializer();
         }, intervalDuration * 1000)
     }
@@ -26,7 +28,13 @@ class TempDB {
     }
 
     get(name) {
+        if (this.events["get"]) {
+            this.events["get"].forEach(c => {
+                c(name);
+            });
+        }
         return this.json.get(name)
+
     }
     /// todo client.on("restartingdb",func)
     has(a) {
@@ -35,22 +43,47 @@ class TempDB {
 
     set(name, value) {
         this.json.set(name, value)
+        if (this.events["set"]) {
+            this.events["set"].forEach(a => {
+                a(name, value);
+            });
+        }
     }
 
     delete(a) {
         this.json.delete(a)
+        if (this.events["delete"]) {
+            this.events["delete"].forEach(c => {
+                c(a);
+            });
+        }
     }
 
     push(a, b) {
         this.json.push(a, b)
+        if (this.events["push"]) {
+            this.events["push"].forEach(c => {
+                c(a, b);
+            });
+        }
     }
 
     add(a, b) {
         this.json.add(a, b)
+        if (this.events["add"]) {
+            this.events["add"].forEach(c => {
+                c(a, b);
+            });
+        }
     }
 
     elementdelete(a, b) {
         this.json.elementdelete(a, b)
+        if (this.events["elementdelete"]) {
+            this.events["elementdelete"].forEach(c => {
+                c(a, b);
+            });
+        }
     }
 
     size() {
@@ -60,6 +93,6 @@ class TempDB {
     length() {
         return Object.keys(this.json.json).length
     }
-
 }
+
 module.exports = TempDB;
